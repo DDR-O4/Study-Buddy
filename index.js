aecb73ac05a47b16e5ed99b5d1bf701607fa043e
RAO.exe <rao.officialengg@gmail.com>
2021-07-10 19:01:35 +0530

    updates

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
+const pagination = require('discord.js-pagination');
+
+const fs = require('fs');
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler => {
+  require(`./handlers/${handler}`)(client, Discord)
+})
 
 client.on('ready', () => {
   console.log('Connected As ' + client.user.tag);
@@ -11,12 +20,5 @@ client.on('message', message =>{
         message.channel.send("Hellow!");
     }
 })
-
-
-
-
-
-
-
 
 client.login(config.token)
