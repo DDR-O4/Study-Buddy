c1230cef962c1eed39e82c76785a5aff0b577417
Ravi Jha <ravikjha7@gmail.com>
2021-07-10 19:07:13 +0530

    Pata Nahi Kya Chal Raha Hai

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
-['command_handler', 'event_handler'].forEach(handler => {
+['command_handler','event_handler'].forEach(handler => {
   require(`./handlers/${handler}`)(client, Discord)
 })
 
