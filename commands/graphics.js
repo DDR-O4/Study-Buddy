9569ebbb4979b8ac004cf5bda9de5e892e46a0d7
Ravi Jha <73632663+ravikjha7@users.noreply.github.com>
2021-07-10 22:43:50 +0530

    Added Code For Graphics Notes

---
+++ commands/graphics.js
@@ -0,0 +1,22 @@
+const pagination = require('discord.js-pagination');
+
+module.exports = {
+    name: 'notes_graphics',
+    description: "Send the Notes Of Graphics",
+    permissions: [],
+
+    execute(message, args, client, cmd, Discord){
+        
+        const page1 = new Discord.MessageEmbed()
+        .setTitle('Graphics Notes')
+        .setURL('https://drive.google.com/drive/u/2/folders/1ajNmgyNfY6N-sidM92kY13zOL0I6iQ1K')
+        .setDescription(`This is the Drive Link For Graphics Notes`)
+        .setImage('https://i.pinimg.com/originals/de/8c/a0/de8ca086d34ed6396b27e039d48bdb3a.png')
+
+        const pages = [
+            page1,
+        ]
+
+        pagination(message, pages)
+    }
+}
